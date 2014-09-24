916c41662f73fbf8d53aa4463494c8a2afc6d50e
whmountains <whmountains@icloud.com>
2014-09-24 13:28:37 -0400

    Dropping custom datepicker
    ...in favor of using scheduler.config.time_step

--- client/views/scheduler.js
+++ client/views/scheduler.js
@@ -1,21 +1,21 @@
-var timeDatepickerPrecise = {
-  render: function(sns) {
-    // sns - section configuration object
-    return 'test';
-  },
-  set_value: function(node, value, ev) {
-    //node - html object related to html defined above
-    //value - value defined by map_to property
-    //ev - event object
-  },
-  get_value: function(node, ev) {
-    //node - html object related to html defined above
-    //event object
-  },
-  focus: function(node) {
-    //node - html object related to html defined above
-  }
-};
+// var timeDatepickerPrecise = {
+//   render: function(sns) {
+//     // sns - section configuration object
+//     return 'test';
+//   },
+//   set_value: function(node, value, ev) {
+//     //node - html object related to html defined above
+//     //value - value defined by map_to property
+//     //ev - event object
+//   },
+//   get_value: function(node, ev) {
+//     //node - html object related to html defined above
+//     //event object
+//   },
+//   focus: function(node) {
+//     //node - html object related to html defined above
+//   }
+// };
 
 var  initScheduler = function () {
 
@@ -25,6 +25,7 @@ var  initScheduler = function () {
                 date:new Date(),
                 mode:"week",
                 hour_size_px: 100,
+                time_step: 0.1,
                 lightbox: {
                   sections: [
                     {
